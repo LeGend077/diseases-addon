33107ef49ab81e2aa576111dc75e4e359f7f34ec
LeGend <98607285+LeGend077@users.noreply.github.com>
2024-12-12 19:40:16 +0530

    updatw

--- packs/BP/scripts/index.js
+++ packs/BP/scripts/index.js
@@ -1,5 +1,5 @@
-import { world } from "@minecraft/server"
-import "./thirst.js"
-import "./food_poisoning.js"
+import { world, system } from "@minecraft/server";
+import "./thirst.js";
+import "./food_poisoning.js";
 
-world.setDynamicProperty('has_foodpoisoning', false)
+world.setDynamicProperty('has_foodpoisoning', false);
