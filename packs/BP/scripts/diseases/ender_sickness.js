3a28377b17e9a5797bb58ebeaa41081a1d1bfeef
TheoristMC <zunheorist@gmail.com>
2024-12-19 08:12:06 +0800

    Added notification when Ender Sickness & Improve it's system.

--- packs/BP/scripts/diseases/ender_sickness.js
+++ packs/BP/scripts/diseases/ender_sickness.js
@@ -1,4 +1,5 @@
 import { world } from "@minecraft/server";
+import { createNotif } from "../index.js";
 
 const enderSicknessProps = {
   enderPearlCount: 0, // Number of ender pearls thrown by the player
@@ -17,14 +18,24 @@ world.afterEvents.itemUse.subscribe((ev) => {
     if (currentTime - enderSicknessProps.lastIncrementTime >= 1000) {
       enderSicknessProps.lastIncrementTime = currentTime;
 
-      if (
-        enderSicknessProps.enderPearlCount < enderSicknessProps.maxEnderPearl &&
-        Math.random() < 0.5 // 50% chance of incrementing
-      ) {
-        enderSicknessProps.enderPearlCount++;
+      if (enderSicknessProps.enderPearlCount < enderSicknessProps.maxEnderPearl) {
+        if (Math.random() < 0.5) { // 50% chance of incrementing
+          enderSicknessProps.enderPearlCount++;
+          console.warn(enderSicknessProps.enderPearlCount);
+        }
       } else {
+        if (!source.getDynamicProperty('has_ender_sickness')) {
+          createNotif(
+            source,
+            "DISEASE:",
+            "You've caught Ender",
+            "textures/items/ender_pearl",
+            "disease"
+          );
+        }
+
+        enderSicknessProps.enderPearlCount = 0;
         source.setDynamicProperty("has_ender_sickness", true);
-        // We just need to implement what to do when player gets ender sickness
       }
     }
   }
